a98ebe50950a9a5c9bc841d4f956edeae7e1397e
yuongboy5896 <yang186107321@163.com>
2021-08-16 09:59:37 +0800

    修改程序

--- mqtt-To-influxdb/main.go
+++ mqtt-To-influxdb/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 	"time"
 
-	"example.com/m/model"
 	"example.com/m/tool"
+
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
@@ -20,51 +18,28 @@ import (
 //
 var quit = false
 var influxdb influxdb2.Client
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-
-	r := ioutil.NopCloser(bytes.NewReader([]byte(msg.Payload())))
-	var mqttMsg model.MqttMsg
-
-	err := tool.Decode(r, &mqttMsg)
-	if err != nil {
-		print("参数解析失败 %s \n", err)
-		return
-	}
-	fmt.Printf("RH %.2f,TMP: %.2f \n", mqttMsg.RH, mqttMsg.TMP)
-	const bucket = "test"
-	const org = "devops"
-
-	filemap := make(map[string]interface{})
-	filemap["rh"] = mqttMsg.RH
-	filemap["tmp"] = mqttMsg.TMP
-	writeAPI := influxdb.WriteAPI(org, bucket)
-	p := influxdb2.NewPoint("home",
-		map[string]string{"unit": "temperature"},
-		filemap,
-		time.Now())
-	// write point asynchronously
-	writeAPI.WritePoint(p)
-	writeAPI.Flush()
-
-}
-
-//
 
 var IOTPubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 	Payloadstr := string(msg.Payload())
+	Topicstr := string(msg.Topic())
+	insertInfluxdb(Payloadstr, Topicstr)
+}
+
+func insertInfluxdb(Payloadstr string, Topicstr string) {
+	//Payloadstr := string(Payload)
+
 	if !strings.Contains(Payloadstr, "post") {
-		fmt.Println("数据不正确")
+		//fmt.Println("数据不正确")
 		return
 	}
-	topic := strings.Split(msg.Topic(), "/")
+	topic := strings.Split(Topicstr, "/")
 	var deviceName string
 	if len(topic) > 6 {
 		deviceName = topic[3]
 	} else {
 		println("没有获取设备名称")
-		return
+		//return
 	}
 	//fmt.Printf("设备名称 %s\n", deviceName)
 	string_slice := strings.Split(Payloadstr, "==========")
@@ -73,7 +48,7 @@ var IOTPubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Messag
 		relust := strings.ReplaceAll(string_slice[1], "=", "")
 		if err := json.Unmarshal([]byte(relust), &data); err != nil {
 			print("参数解析失败 %s \n", err)
-			return
+			//return
 		}
 	}
 
@@ -83,18 +58,19 @@ var IOTPubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Messag
 	tagmap["IP"] = string_slice[0]
 
 	params := data["params"].(map[string]interface{})
-	if params == nil {
-		println("转行失败")
-		return
+	if params != nil {
+
+		influxdb = influxdb2.NewClient(Infurl, Token)
+		defer influxdb.Close()
+		writeAPI := influxdb.WriteAPI(org, bucket)
+		p := influxdb2.NewPoint(deviceName,
+			tagmap,
+			params,
+			time.Now())
+		// write point asynchronously
+		writeAPI.WritePoint(p)
+		writeAPI.Flush()
 	}
-	writeAPI := influxdb.WriteAPI(org, bucket)
-	p := influxdb2.NewPoint(deviceName,
-		tagmap,
-		params,
-		time.Now())
-	// write point asynchronously
-	writeAPI.WritePoint(p)
-	writeAPI.Flush()
 
 }
 
@@ -108,6 +84,8 @@ var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err
 		fmt.Printf("Connect lost: %v", err)
 	}
 }
+var Infurl string
+var Token string
 
 func main() {
 
@@ -116,8 +94,10 @@ func main() {
 	if err != nil {
 		println(err)
 	}
-	influxdb = influxdb2.NewClient(cfg.Infludb.Infurl, cfg.Infludb.Token)
-	defer influxdb.Close()
+	Infurl = cfg.Infludb.Infurl
+	Token = cfg.Infludb.Token
+	//influxdb = influxdb2.NewClient(cfg.Infludb.Infurl, cfg.Infludb.Token)
+	//defer influxdb.Close()
 
 	var broker = cfg.Mqtt.Mqttip
 	var port = cfg.Mqtt.Mqttport
@@ -141,15 +121,18 @@ func main() {
 
 	subIotMultiple(client, filters)
 	for {
-		if !client.IsConnected() {
+		if quit {
 			client.Disconnect(250)
 			fmt.Println("程序退出")
 			os.Exit(3)
 		}
+		time.Sleep(2 * time.Second)
 	}
 }
 
 func subIotMultiple(client mqtt.Client, filters map[string]byte) {
-	token := client.SubscribeMultiple(filters, nil)
-	token.Wait()
+	if token := client.SubscribeMultiple(filters, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
 }
